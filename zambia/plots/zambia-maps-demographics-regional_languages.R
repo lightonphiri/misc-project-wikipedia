8cf669b35e64d102a20d2a6aaabbd1f31265ef57
Lighton Phiri <lighton.phiri@gmail.com>
2016-01-27 01:09:10 +0200

    Zambia: Demographics
    Added regional languages density plot

--- zambia/plots/zambia-maps-demographics-regional_languages.R
+++ zambia/plots/zambia-maps-demographics-regional_languages.R
@@ -1,4 +1,3 @@
-library(Cairo)
 library(ggplot2)
 require(extrafont)
 library(maps)
@@ -10,11 +9,24 @@ library(maptools)
 if (!require(gpclib)) install.packages("gpclib", type="source")
 gpclibPermit()
 
-census.df <- read.table("../data/ZMB_census.txt", header=TRUE, sep=",")
+languages.df <- read.table(text="Region,Language,Percent
+Central,Bemba,31.8
+Copperbelt,Bemba,83.9
+Eastern,Chewa,34.6
+Luapula,Bemba,71.3
+Lusaka,Nyanja,61.9
+Muchinga,Bemba,46.9
+Northern,Bemba,69.2
+North-Western,Lunda,33.8
+Southern,Tonga,74.7
+Western,Lozi,69.6
+", header=TRUE, sep=",")
+
 
 zambia.adm1.spdf <- readOGR(dsn="../data", layer="ZMB_adm1")
 zambia.adm1.df <- fortify(zambia.adm1.spdf, region = "NAME_1")
-zambia.adm1.df <- merge(zambia.adm1.df, census.df, by.y="KEY", by.x="id")
+#zambia.adm1.df <- merge(zambia.adm1.df, census.df, by.y="KEY", by.x="id")
+zambia.adm1.df <- merge(zambia.adm1.df, languages.df, by.y="Region", by.x="id")
 
 
 zambia.adm1.centroids.df <- data.frame(long = coordinates(zambia.adm1.spdf)[, 1], 
@@ -23,19 +35,19 @@ zambia.adm1.centroids.df <- data.frame(long = coordinates(zambia.adm1.spdf)[, 1]
 # Get names and id numbers corresponding to administrative areas
 zambia.adm1.centroids.df[, 'ID_1'] <- zambia.adm1.spdf@data[,'ID_1']
 zambia.adm1.centroids.df[, 'NAME_1'] <- zambia.adm1.spdf@data[,'NAME_1']
+zambia.adm1.centroids.df <- merge(zambia.adm1.centroids.df, languages.df, by.y="Region", by.x="NAME_1")
 
-#png(filename = "zambia-maps-geography_climate-provinces.png")
 
 pp <- ggplot(zambia.adm1.df, aes(x = long, y = lat, group = group)) + 
-  geom_polygon(aes(fill = id)) +
-  #geom_polygon(aes(fill = "wheat")) +
+  geom_polygon(aes(fill = Language)) +
   geom_path(colour = 'black', linetype = 1) + 
-  geom_text(data = zambia.adm1.centroids.df, aes(label = toupper(ID_1), x = long, y = lat, group = ID_1), size=15*0.35, family = "CM Roman", fontface = "bold") + 
+  #geom_text(data = zambia.adm1.centroids.df, aes(label = Percent, x = long, y = lat, group = NAME_1), size=9*0.35, family = "CM Roman", fontface = "bold") + 
   labs(x=" ", y=" ") + 
   theme_bw() +  
   scale_fill_brewer(palette = "RdYlBu") +
-  #scale_fill_manual(values = c("#e6f598", "#e6f598", "#e6f598", "#e6f598", "#e6f598", "#e6f598", "#e6f598", "#e6f598", "#e6f598", "#e6f598")) +
-  coord_map() + 
+  #scale_fill_manual(values = c("#f0f0f0", "#f0f0f0", "#f0f0f0", "#f0f0f0", "#f0f0f0", "#f0f0f0", "#f0f0f0", "#f0f0f0", "#f0f0f0", "#f0f0f0")) +
+  coord_map() +
+  #guides(fill = guide_legend(nrow = 1, byrow = TRUE)) +
   theme(
         text=element_text(family="CM Roman"),
         panel.background = element_blank(),
@@ -44,13 +56,14 @@ pp <- ggplot(zambia.adm1.df, aes(x = long, y = lat, group = group)) +
         panel.grid.major=element_blank(),
         panel.border = element_blank(),
         plot.margin=unit(c(-0.7,-0.8,-1.3,-1.3),"cm"),
+        legend.title = element_blank(),
         legend.position = "none",
         axis.ticks = element_blank(), 
         axis.text.x = element_blank(), 
         axis.text.y = element_blank()
         )
 
-svg("zambia-maps-geography_climate-provinces.svg", bg = 'transparent')
+svg("zambia-maps-demographics-regional_languages.svg", bg = 'transparent')
 
 pp
         
